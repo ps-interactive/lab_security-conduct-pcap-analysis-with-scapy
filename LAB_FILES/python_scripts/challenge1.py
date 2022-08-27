9fc9c0d23c3b79f691fd0f4f2310dff33dae07cf
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2022-08-27 17:31:36 +0800

    Challenge Code

---
+++ LAB_FILES/python_scripts/challenge1.py
@@ -0,0 +1,12 @@
+# import the scapy library
+from scapy.all import *
+
+# read the packet capture file
+packets = rdpcap("/home/ubuntu/lab/LAB_FILES/exercise1.pcapng")
+
+# loop through each packet
+for pkt in packets:
+  if IP in pkt:
+    # print source and destination ip addresses and ports
+    print(str(pkt[IP].src)+"-->"+str(pkt[IP].dst)+" : "+str(pkt[IP].dport)+"-->"+str(pkt[IP].sport))
+
