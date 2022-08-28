7a70582dc82d9f84dfaa30bf09b5b4586f8cface
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2022-08-28 10:55:42 +0800

    Added dns script

---
+++ LAB_FILES/python_scripts/challenge3-1.py
@@ -0,0 +1,11 @@
+# import scapy
+from scapy.all import *
+
+# read the packet capture file
+packets = rdpcap("/home/ubuntu/lab/LAB_FILES/exercise3.pcap")
+
+# loop through each packet
+for pkt in packets:
+  if pkt.haslayer(DNS):
+    # if a packet is DNS protocol, print it
+    print(pkt.show())
